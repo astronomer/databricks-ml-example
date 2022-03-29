d733cdf68830fce571fb702e04fb4bc850cf09b8
Faisal <fhoda@users.noreply.github.com>
2022-03-29 14:01:25 -0500

    use native databricks_hook functions for notebook output

--- dags/databricks-ml-example.py
+++ dags/databricks-ml-example.py
@@ -4,16 +4,12 @@ import logging
 from airflow.decorators import task, dag
 from airflow.providers.databricks.hooks.databricks import DatabricksHook
 from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator
-# from astronomer.providers.databricks.operators.databricks import DatabricksSubmitRunOperatorAsync
-
-from include.databricks_helper import get_notebook_output
 
 import mlflow
 
 docs = """
 Demonstrates orchestrating ML pipelines executed on Databricks with Airflow
 """
-
 
 
 @dag(
@@ -32,7 +28,7 @@ def databricks_ml_example():
         task_id='ingest_notebook_task',
         existing_cluster_id="{{ var.value.databricks_cluster_id }}",
         notebook_task=ingest_notebook
-        )
+    )
  
     feature_engineering_notebook = {
         'notebook_path': "/Users/{{ var.value.databricks_user }}/feature-eng_census-pred",
@@ -42,7 +38,7 @@ def databricks_ml_example():
         task_id='feature_engineering_notebook_task',
         existing_cluster_id="{{ var.value.databricks_cluster_id }}",
         notebook_task=feature_engineering_notebook
-        )
+    )
 
     train_notebook = {
         'notebook_path': "/Users/{{ var.value.databricks_user }}/LightGBM-Census-Classifier"
@@ -53,7 +49,7 @@ def databricks_ml_example():
         existing_cluster_id="{{ var.value.databricks_cluster_id }}",
         notebook_task=train_notebook,
         do_xcom_push=True
-        )
+    )
 
     
     @task
@@ -61,7 +57,9 @@ def databricks_ml_example():
 
         logging.info(f'Training notebook run_id: {databricks_run_id}')
 
-        model_uri = get_notebook_output(databricks_run_id)
+        databricks_hook = DatabricksHook()
+        model_uri = databricks_hook.get_run_output(databricks_run_id)['notebook_output']['result']
+        logging.info(f'Model URI: {model_uri}')
         
         model_version = mlflow.register_model(model_uri, 'census_pred')
 
@@ -74,7 +72,7 @@ def databricks_ml_example():
             name=model_version.name,
             version=model_version.version,
             stage="Staging"
-            )
+        )
 
 
     ingest >> feauture_engineering >> train
--- include/databricks_helper.py
+++
@@ -1,8 +0,0 @@
-from airflow.providers.databricks.hooks.databricks import DatabricksHook
-
-def get_notebook_output(databricks_run_id: str):
-    databricks_hook = DatabricksHook()
-
-    response = databricks_hook._do_api_call(("GET", f'api/2.0/jobs/runs/get-output?run_id={databricks_run_id}'), {})
-    
-    return response['notebook_output']['result']
