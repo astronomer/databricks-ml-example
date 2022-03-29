c2c9429c3cc8a3c45b9d0d9d0379d0adf00a508e
Faisal <fhoda@users.noreply.github.com>
2022-03-28 21:18:21 -0500

    example notebooks and refactor of dag

--- README.md
+++ README.md
@@ -2,22 +2,24 @@
 
 These DAGs give basic examples on how to use Airflow to orchestarte your ML tasks in Databricks. The Databricks code is in a Databricks notebook for which you can find descriptions of below.
 
-1. **databricks-automl-example.py** - Runs an end to end data ingest to model publishing pipeline.
+1. **databricks-ml-example.py** - Runs an end to end data ingest to model publishing pipeline with the following tasks:
     - **ingest:** Pulls data from BigQuery and does some basic cleaning and transformations then saves it to Delta Lake.
     - **feature engineering:**  Extract features for model and save output to the Feature Store.
     - **train:** Train model
     - **register:** register model to mlflow
 
- ## Requirements
+## Requirements
 
- ### Bigquery
+### Bigquery
  - [Service account with correct permsissions](https://docs.databricks.com/data/data-sources/google/bigquery.html#step-1-set-up-google-cloud) to use with Databricks cluster
 
- ### Databricks
- - [Authentication token](https://docs.databricks.com/dev-tools/api/latest/authentication.html) (if you don't want to use a username and password to authenticate from Airflow)
- - Existing cluster [setup with GCP credentials](https://docs.databricks.com/dev-tools/api/latest/authentication.html) (you can use an on demand cluster but you will need to supply it the GCP credentials accordingly)
+### Databricks
+  - [Authentication token](https://docs.databricks.com/dev-tools/api/latest/authentication.html) (if you don't want to use a username and password to authenticate from Airflow)
+  - Existing cluster [setup with GCP credentials](https://docs.databricks.com/data/data-sources/google/bigquery.html#create-a-google-service-account-for-databricks) (you can use an on demand cluster but you will need to supply it the GCP credentials accordingly)
+  - Notebooks for each task.
+     - For `databricks-ml-example.py` you can use the notebooks in the `example_notebooks` folder which have been provided in this repo to get started.
 
- ### Airflow
+### Airflow
  - [Databricks connection](https://airflow.apache.org/docs/apache-airflow-providers-databricks/stable/connections/databricks.html)
  - MLflow environment variables in your .env
      - MLFLOW_TRACKING_URI=databricks
--- dags/databricks-ml-example.py
+++ dags/databricks-ml-example.py
@@ -6,11 +6,17 @@ from airflow.providers.databricks.hooks.databricks import DatabricksHook
 from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator
 # from astronomer.providers.databricks.operators.databricks import DatabricksSubmitRunOperatorAsync
 
+from include.databricks_helper import get_notebook_output
+
 import mlflow
 
 docs = """
 Demonstrates orchestrating ML pipelines executed on Databricks with Airflow
 """
+
+DATABRICKS_USER='<your username>'
+DATABRICKS_NOTEBOOK_PATH=f'/Users/{DATABRICKS_USER}'
+DATABRICKS_CLUSTER_ID='<databricks cluster id to attach notebooks to>'
 
 @dag(
     start_date=datetime(2022, 1, 1),
@@ -21,33 +27,33 @@ Demonstrates orchestrating ML pipelines executed on Databricks with Airflow
 def databricks_ml_example():
 
     ingest_notebook = {
-        'notebook_path': '/Users/<your username>/<BigQuery-to-Databricks-notebook>',
+        'notebook_path': f'{DATABRICKS_NOTEBOOK_PATH}/BigQuery-to-Databricks',
     }
 
     ingest = DatabricksSubmitRunOperator(
         task_id='ingest_notebook_task',
-        existing_cluster_id='0224-221140-suj0ngd4',
+        existing_cluster_id=DATABRICKS_CLUSTER_ID,
         notebook_task=ingest_notebook
         )
  
     feauture_engineering_notebook = {
-        'notebook_path': '/Users/<your username>/<feauture-eng-notebook>',
+        'notebook_path': '{DATABRICKS_NOTEBOOK_PATH}/feauture-eng_census-pred',
     }
 
     feauture_engineering = DatabricksSubmitRunOperator(
         task_id='feature_engineering_notebook_task',
-        existing_cluster_id='0224-221140-suj0ngd4',
+        existing_cluster_id=DATABRICKS_CLUSTER_ID,
         notebook_task=feauture_engineering_notebook
         )
 
 
     train_notebook = {
-        'notebook_path': '/Users/<your username>/databricks_automl/<model-traning-notebook>'
+        'notebook_path': '{DATABRICKS_NOTEBOOK_PATH}/LightGBM-Census-Classifier'
     }
 
     train = DatabricksSubmitRunOperator(
         task_id='train_notebook_task',
-        existing_cluster_id='0224-221140-suj0ngd4',
+        existing_cluster_id=DATABRICKS_CLUSTER_ID,
         notebook_task=train_notebook,
         do_xcom_push=True
         )
@@ -57,24 +63,20 @@ def databricks_ml_example():
     def register_model(ti=None):
 
         databricks_run_id = ti.xcom_pull(task_ids='train_notebook_task', key='run_id')
-        logging.info(databricks_run_id)
+        logging.info(f'Training notebook run_id: {databricks_run_id}')
 
-        databricks_hook = DatabricksHook()
-
-        response = databricks_hook._do_api_call(("GET", f'api/2.0/jobs/runs/get-output?run_id={databricks_run_id}'), {})
+        model_uri = get_notebook_output(databricks_run_id)
         
-        model_uri = response['notebook_output']['result']
-        
-        mv = mlflow.register_model(model_uri, 'census_pred')
+        model_version = mlflow.register_model(model_uri, 'census_pred')
 
-        logging.info(f'Name: {mv.name}')
-        logging.info(f'Version: {mv.version}')
+        logging.info(f'Name: {model_version.name}')
+        logging.info(f'Version: {model_version.version}')
 
         client = mlflow.tracking.MlflowClient()
 
         client.transition_model_version_stage(
-            name=mv.name,
-            version=mv.version,
+            name=model_version.name,
+            version=model_version.version,
             stage="Staging"
             )
 
