ae14678ec70436b2f9c207beb5ee20329a249b49
Faisal <fhoda@users.noreply.github.com>
2022-04-07 17:23:58 -0500

    add notebook_task values directly in operator and switch to pendulum

--- dags/databricks-automl-example.py
+++ dags/databricks-automl-example.py
@@ -12,44 +12,38 @@ Demonstrates orchestrating ML pipelines executed on Databricks with Airflow
     start_date=datetime(2022, 1, 1),
     schedule_interval=None,
     catchup=False,
-    doc_md=docs
+    doc_md=docs,
+    default_args={
+        'existing_cluster_id': "{{ var.value.databricks_cluster_id }}"
+    }
 )
 def databricks_automl_example():
 
-    ingest_notebook = {
-        'notebook_path': "/Users/{{ var.value.databricks_user }}/BigQuery_to_Databricks",
-    }
-
     ingest = DatabricksSubmitRunOperator(
         task_id='ingest_notebook_task',
-        existing_cluster_id="{{ var.value.databricks_cluster_id }}",
-        notebook_task=ingest_notebook
+        notebook_task={
+            'notebook_path': "/Users/{{ var.value.databricks_user }}/BigQuery_to_Databricks"
+        }
     )
- 
-    feature_engineering_notebook = {
-        'notebook_path': "/Users/{{ var.value.databricks_user }}/feature-eng_census-pred",
-    }
 
     feauture_engineering = DatabricksSubmitRunOperator(
         task_id='feature_engineering_notebook_task',
-        existing_cluster_id="{{ var.value.databricks_cluster_id }}",
-        notebook_task=feature_engineering_notebook
+        notebook_task={
+            'notebook_path': "/Users/{{ var.value.databricks_user }}/feature-eng_census-pred",
+        }
     )
-
-    train_automl_notebook = {
-        'notebook_path': "/Users/{{ var.value.databricks_user }}/census_automl",
-        'base_parameters': {
-            'target_variable': 'never_married',
-            'features_database': 'census_data',
-            'features_table': 'census_adult_income_features',
-            'data_dir': 'dbfs:/automl/adult'
-        }         
-    }
 
     train_automl = DatabricksSubmitRunOperator(
         task_id='train_notebook_task',
-        existing_cluster_id="{{ var.value.databricks_cluster_id }}",
-        notebook_task=train_automl_notebook,
+        notebook_task={
+            'notebook_path': "/Users/{{ var.value.databricks_user }}/census_automl",
+            'base_parameters': {
+                'target_variable': 'never_married',
+                'features_database': 'census_data',
+                'features_table': 'census_adult_income_features',
+                'data_dir': 'dbfs:/automl/adult'
+            }
+        },
         do_xcom_push=True
     )
 
